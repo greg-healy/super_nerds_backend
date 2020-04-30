7aec4dd09045b72c8ff4612aad221ed7fe3c42f1
Gavin S <gavinslusher@gmail.com>
2020-04-29 21:05:01 -0400

    Added some comments / prepping for pair programing

--- flask_api/__init__.py
+++ flask_api/__init__.py
@@ -8,23 +8,27 @@ from .commands import create_tables
 from .routes.main import main
 from .routes.auth import auth
 
+
 def create_app(config_file='settings.py'):
-	app = Flask(__name__)
+    app = Flask(__name__)
 
-	# Takes environment variables from the .env file and assigns them
-	# to values for use by the app 
-	app.config.from_pyfile(config_file)
+    # Takes environment variables from the .env file and assigns them
+    # to values for use by the app
+    app.config.from_pyfile(config_file)
 
-	db.init_app(app)
+    # Instantiating the database
+    db.init_app(app)
 
-	# Brings the functionality of the routes defined in routes.py using
-	# the "main" blueprint and applies them to the app defined in here
-	app.register_blueprint(main)
-	app.register_blueprint(auth)
+    # Brings the functionality of the routes defined in routes folder using
+    # the "main" and "auth" blueprints and applies them to the app defined
+    # in here
 
-	# Running `flask create_tables` will create the tables for us
-	# Must be run again if more tables are created/changed
-	# On Heroku must also run `flask create_tables` in console
-	app.cli.add_command(create_tables)
+    app.register_blueprint(main)
+    app.register_blueprint(auth)
 
-	return app
+    # Running `flask create_tables` will create the tables for us
+    # Must be run again if more tables are created/changed
+    # On Heroku must also run `flask create_tables` in console
+    app.cli.add_command(create_tables)
+
+    return app
--- flask_api/commands.py
+++ flask_api/commands.py
@@ -1,12 +1,13 @@
 # This file is used to create the tables in the database
 
-import click
-from flask.cli import with_appcontext
+import click  # Library that Flask uses for command line functionality
+from flask.cli import with_appcontext  # lets commands use app config
 
 from .extensions import db
 from .models import Users, Banks, Transactions
 
-@click.command(name='create_tables')
+
+@click.command(name='create_tables')  # defines the command
 @with_appcontext
-def create_tables():
-	db.create_all()
+def create_tables():  # whatever you want to execute when you run the command
+    db.create_all()
