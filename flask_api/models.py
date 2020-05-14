e468eb732ed45213a826e2f3edd8ff714eb3c2fc
Gavin S <gavinslusher@gmail.com>
2020-05-14 19:08:31 -0400

    Made users and banks one-one. Needs create_tables update

--- flask_api/models.py
+++ flask_api/models.py
@@ -3,16 +3,22 @@ from .extensions import db
 
 
 class Users(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(32))
     last_name = db.Column(db.String(32))
     email = db.Column(db.String(128))
     password = db.Column(db.String(32))
+    account_balance = db.Column(db.Float)
+
+    # One to One Relationship
+    banks = db.relationship('Banks', backref='users', uselist=False)
 
 
 class Banks(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    account_number = db.Column(db.Integer, primary_key=True,
+                               autoincrement=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
 
 
 class Transactions(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    transaction_id = db.Column(db.Integer, primary_key=True)
